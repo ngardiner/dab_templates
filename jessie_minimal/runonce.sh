62b582db6c46de4fcffe0761cba8ee9c5944c82d
Nathan Gardiner <ngardiner@gmail.com>
2017-02-19 03:32:01 -0500

    Jessie templates tested in full

--- jessie_minimal/runonce.sh
+++ jessie_minimal/runonce.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+### BEGIN INIT INFO
+# Provides:          firstboot
+# Required-Start:    $local_fs
+# Required-Stop:
+# Should-Start:      $network $portmap nfs-common  udev-mtab
+# Default-Start:     S
+# Default-Stop:
+# Short-Description: First-boot system customization routines
+# Description:       Provides first-boot system customization for
+#                    proxmox container templates.
+#                    Removes itself entirely when done.
+### END INIT INFO
+
+# Put first boot routines here
 
 # Once the script has completed execution, delete ourselves
+update-rc.d firstboot disable
 rm $0
 
 exit 0
--- xenial_standard/README.md
+++ xenial_standard/README.md
@@ -2,5 +2,5 @@
 
 - Built from a standard debootstrap install of Ubuntu Xenial
 - Adds any customizations such as root login enabled or SSH keys from ../Makefile.global
-- Total uncompressed image size is *0 MB*
-- Total compressed image size is *0 MB*
+- Total uncompressed image size is *579 MB*
+- Total compressed image size is *191 MB*
