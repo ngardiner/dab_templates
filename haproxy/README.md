2d6daff9c86ab34d42034683a3675ec79f7d6391
Nathan Gardiner <ngardiner@gmail.com>
2017-02-21 00:29:50 -0500

    HAProxy configuration provides SSL & Status Check out of the box

--- haproxy/README.md
+++ haproxy/README.md
@@ -1,7 +1,12 @@
 # HAProxy
 
+<a href="http://www.haproxy.org">HAProxy</a> is a high availability solution for load balancing services. 
+
+This template provides a bootstrapped HAProxy system with the following pre-configured features:
+
 - Builds a HAProxy Server with Certbot for SSL Certificate management
   - Uses the excellent <a href="https://github.com/janeczku/haproxy-acme-validation-plugin">HAProxy ACME Validation Plugin</a> by <a href="https://github.com/janeczku">janeczku</a> to provide zero-downtime Letsencrypt SSL validation and renewal.
+  - Works out-of-the-box with a self-signed SSL certificate.
 - HAProxy server will provide HTTP and HTTPS, with HTTP redirecting to HTTPS
 - HAProxy server will serve a static web page for status.check.localhost which can be used to check the HAProxy status from other platforms such as Amazon Route 53.
 - Adds any customizations such as root login enabled or SSH keys from ../Makefile.global
--- haproxy/runonce.sh
+++ haproxy/runonce.sh
@@ -17,6 +17,27 @@ case "$1" in
 
     # Put first boot routines here
 
+    # Create self-signed fallback ssl key
+    mkdir -p /etc/ssl/haproxy
+    mkdir -p /etc/ssl/haproxy/pem
+    echo "AU" >> /etc/ssl/haproxy/haproxy.ans
+    echo "VIC" >> /etc/ssl/haproxy/haproxy.ans
+    echo "Melbourne" >> /etc/ssl/haproxy/haproxy.ans
+    echo "Load Balancer" >> /etc/ssl/haproxy/haproxy.ans
+    echo "Load Balancer" >> /etc/ssl/haproxy/haproxy.ans
+    echo "*.localhost" >> /etc/ssl/haproxy/haproxy.ans
+    echo "loadbalancer@localhost" >> /etc/ssl/haproxy/haproxy.ans
+    echo >> /etc/ssl/haproxy/haproxy.ans
+    echo >> /etc/ssl/haproxy/haproxy.ans
+    openssl genrsa -out /etc/ssl/haproxy/haproxy.key 2048
+    cat /etc/ssl/haproxy/haproxy.ans |  openssl req -new -key /etc/ssl/haproxy/haproxy.key -out /etc/ssl/haproxy/haproxy.csr
+    openssl x509 -req -days 3650 -in /etc/ssl/haproxy/haproxy.csr \
+                    -signkey /etc/ssl/haproxy/haproxy.key \
+                    -out /etc/ssl/haproxy/haproxy.crt
+
+    # Create integrated PEM file
+    cat /etc/ssl/haproxy/haproxy.crt /etc/ssl/haproxy/haproxy.key > /etc/ssl/haproxy/pem/haproxy.pem
+
     # Once the script has completed execution, delete ourselves
     update-rc.d firstboot disable
     rm $0
