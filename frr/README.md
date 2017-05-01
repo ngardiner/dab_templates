bcf71300ffecc17e3f2639463cdc33f6f9442507
ngardiner <ngardiner@gmail.com>
2017-05-01 19:15:06 +1000

    First test of FRR compilation

---
+++ frr/README.md
@@ -0,0 +1,10 @@
+# Free Range Routing
+
+From the developers of quagga comes an integrated Linux based Router project.
+
+The FRR project, which is brand new as of April 2017, 
+
+- Built from a standard debootstrap install of Ubuntu Xenial
+- Adds any customizations such as root login enabled or SSH keys from ../Makefile.global
+- Total uncompressed image size is *581 MB*
+- Total compressed image size is *191 MB*
--- frr/custom.sh
+++ frr/custom.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cd /usr/src
-git clone https://github.com/frrouting/frr.git frr
-
-cd frr
+# Build sources
+cd /usr/src/frr
 ./bootstrap.sh
 ./configure \
     --prefix=/usr \
@@ -25,5 +23,34 @@ cd frr
     --enable-tcp-zebra \
     --enable-fpm \
     --with-pkg-git-version
+
+# Make the binaries
+make
+make check
+make install
+
+# Create empty FRR configuration files
+install -m 755 -o frr -g frr -d /var/log/frr
+install -m 775 -o frr -g frrvty -d /etc/frr
+install -m 640 -o frr -g frr /dev/null /etc/frr/zebra.conf
+install -m 640 -o frr -g frr /dev/null /etc/frr/bgpd.conf
+install -m 640 -o frr -g frr /dev/null /etc/frr/ospfd.conf
+install -m 640 -o frr -g frr /dev/null /etc/frr/ospf6d.conf
+install -m 640 -o frr -g frr /dev/null /etc/frr/isisd.conf
+install -m 640 -o frr -g frr /dev/null /etc/frr/ripd.conf
+install -m 640 -o frr -g frr /dev/null /etc/frr/ripngd.conf
+install -m 640 -o frr -g frr /dev/null /etc/frr/pimd.conf
+install -m 640 -o frr -g frr /dev/null /etc/frr/ldpd.conf
+install -m 640 -o frr -g frr /dev/null /etc/frr/nhrpd.conf    
+install -m 640 -o frr -g frrvty /dev/null /etc/frr/vtysh.conf
+
+# Enable MPLS
+cat << EOFA >> /etc/sysctl.conf
+# Enable MPLS Label processing on all interfaces
+net.mpls.conf.eth0.input=1
+net.mpls.conf.eth1.input=1
+net.mpls.conf.eth2.input=1
+net.mpls.platform_labels=100000
+EOFA
 
 exit 0
