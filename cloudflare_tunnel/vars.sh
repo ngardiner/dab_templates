8eeab6ddd3176b4d86d0b6eb483edbe56bc741a5
Nathan Gardiner <ngardiner@gmail.com>
2025-05-30 14:16:44 +0800

    Add variables for Cloudflare Tunnel template

---
+++ cloudflare_tunnel/vars.sh
@@ -0,0 +1,14 @@
+# Cloudflare Tunnel Variables
+
+# Cloudflare Account Details
+CF_ACCOUNT_ID=""
+CF_API_TOKEN=""
+
+# Tunnel Configuration
+TUNNEL_NAME="dab-cloudflare-tunnel"
+TUNNEL_HOSTNAME=""
+TARGET_SERVICE_URL=""
+
+# Docker Compose Specific
+CONTAINER_NAME="cloudflare-tunnel"
+DOCKER_NETWORK="bridge"
